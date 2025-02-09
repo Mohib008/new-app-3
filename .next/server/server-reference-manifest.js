3a1d5788fce86c7ac168914346a3891a0a789eca
Mohib Arsala <mohib.0008@gmail.com>
2025-02-09 14:30:11 -0500

    new update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lDlVpoz9fXycjYPCFyZuuLOCQYYFHUaqCg+l8O/Ln4s=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JfZo/Bd7VjDHOG7Y+zRYBbkTFiTJ/th2aso/ZLj8Z2o=\"\n}"
